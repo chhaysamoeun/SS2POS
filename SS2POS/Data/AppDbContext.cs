c742b0b85a89d743068830f5bca2a06e29aaf4bf
Samoeun <samoeunchh@gmail.com>
2023-01-02 09:47:09 +0700

    Add Customer and Category

---
+++ SS2POS/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using SS2POS.Models;
+
+namespace SS2POS.Data
+{
+	public class AppDbContext:DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options)
+			:base(options)
+		{
+		}
+		public DbSet<Category> Category { get; set; }
+		public DbSet<Customer> Customer { get; set; }
+	}
+}
+
